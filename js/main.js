79eaff3742d483a59d477829c53dc1d4b152d680
Scorpion4207 <146665854+Scorpion4207@users.noreply.github.com>
2024-12-05 01:39:01 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ import {
 } from "./element.js";
 import { cityNameError, errorIsSameNameCities } from "./error.js";
 
-function fetchCityWeather(name = 'Москва') {
-  const url = `${ELEMENT_URL.SERVER_URL}?q=${name}&units=metric&appid=${ELEMENT_URL.API_KEY}&lang=ru`;
+function fetchCity(name, request) {
+  const url = `${ELEMENT_URL.SERVER_URL}${request}?q=${name}&units=metric&appid=${ELEMENT_URL.API_KEY}&lang=ru`;
   return fetch(url).then((response) => {
     if (response.status === 200) {
       return response.json();
@@ -17,19 +17,37 @@ function fetchCityWeather(name = 'Москва') {
   });
 }
 
-
+function showTime(data, elementUi){
+  const date = new Date(data * 1000);
+  const hours = String(date.getHours()).length === 1 ? "0" + date.getHours(): date.getHours() ;
+  const minutes = String(date.getMinutes()).length === 1 ? "0" + date.getMinutes():  date.getMinutes() ;
+  return elementUi.textContent = hours + ':' + minutes
+}
 
 function showWeatherOfCity(name = ELEMENT_UI.SEARCH_CITY.value) {
-  fetchCityWeather(name)
+  fetchCity(name, 'weather')
     .then((data) => {
       ELEMENT_UI.SHOW_CITY.textContent = data.name;
       ELEMENT_UI.TEMPETATURE.textContent = Math.round(data.main.temp);
-      ELEMENT_UI.IMG.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+      ELEMENT_UI.IMG.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      ELEMENT_UI.FEELS_LIKE.textContent = Math.round(data.main.feels_like) ;
+      showTime(data.sys.sunrise, ELEMENT_UI.SUNRISE);
+      showTime(data.sys.sunset, ELEMENT_UI.SUNSET);
     })
     .catch((error) => alert(error));
 }
 
-showWeatherOfCity('Москва')
+function showScheduleOfCity(name = ELEMENT_UI.SEARCH_CITY.value){
+  fetchCity(name, 'forecast')
+  .then((data) => {
+    for(let i = 0; i < ELEMENT_UI.SCHEDULES.length; i++){
+      showTime(data.list[i].dt, ELEMENT_UI.SCHEDULE_TIMES[i])
+      ELEMENT_UI.SCHEDULE_TEMP[i].textContent = Math.round(data.list[i].main.temp) 
+      ELEMENT_UI.SCHEDULE_FEELS_LIKE[i].textContent = Math.round(data.list[i].main.feels_like) 
+      ELEMENT_UI.SCHEDULE_ICON[i].src =`https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png` 
+    }
+  })
+}
 
 
 function addFavouriteCities() {
@@ -73,6 +91,7 @@ function useFavouriteCities() {
         target.parentNode.classList.add("use");
         const name = document.querySelector(".use").textContent;
         showWeatherOfCity(name);
+        showScheduleOfCity(name);
         createFavoritesItem();
       }
     });
@@ -89,6 +108,7 @@ function createFavoritesItem() {
 ELEMENT_UI.FORM.addEventListener("submit", (e) => {
   e.preventDefault();
   showWeatherOfCity();
+  showScheduleOfCity();
   deleteFavouriteCities();
   useFavouriteCities();
   ELEMENT_UI.SEARCH_CITY.value = "";
