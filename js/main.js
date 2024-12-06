d5fa6d2c6c3de6865085f8324850174826f37035
Scorpion4207 <telitsin.andrei2015@yandex.ru>
2024-12-06 22:41:57 +0700

    added save and schedule

--- js/main.js
+++ js/main.js
@@ -6,8 +6,16 @@ import {
 } from "./element.js";
 import { cityNameError, errorIsSameNameCities } from "./error.js";
 
-function fetchCityWeather(name = 'Москва') {
-  const url = `${ELEMENT_URL.SERVER_URL}?q=${name}&units=metric&appid=${ELEMENT_URL.API_KEY}&lang=ru`;
+function callAllFunctions() {
+  saveСurrentCity();
+  saveFavoriteCitiese();
+  createFavoritesItem();
+  deleteFavouriteCities();
+  useFavouriteCities();
+}
+
+function fetchCity(name, request) {
+  const url = `${ELEMENT_URL.SERVER_URL}${request}?q=${name}&units=metric&appid=${ELEMENT_URL.API_KEY}&lang=ru`;
   return fetch(url).then((response) => {
     if (response.status === 200) {
       return response.json();
@@ -17,20 +25,48 @@ function fetchCityWeather(name = 'Москва') {
   });
 }
 
-
+function showTime(data, elementUi) {
+  const date = new Date(data * 1000);
+  const hours =
+    String(date.getHours()).length === 1
+      ? "0" + date.getHours()
+      : date.getHours();
+  const minutes =
+    String(date.getMinutes()).length === 1
+      ? "0" + date.getMinutes()
+      : date.getMinutes();
+  return (elementUi.textContent = hours + ":" + minutes);
+}
 
 function showWeatherOfCity(name = ELEMENT_UI.SEARCH_CITY.value) {
-  fetchCityWeather(name)
+  fetchCity(name, "weather")
     .then((data) => {
       ELEMENT_UI.SHOW_CITY.textContent = data.name;
       ELEMENT_UI.TEMPETATURE.textContent = Math.round(data.main.temp);
-      ELEMENT_UI.IMG.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+      ELEMENT_UI.IMG.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      ELEMENT_UI.FEELS_LIKE.textContent = Math.round(data.main.feels_like);
+      showTime(data.sys.sunrise, ELEMENT_UI.SUNRISE);
+      showTime(data.sys.sunset, ELEMENT_UI.SUNSET);
     })
     .catch((error) => alert(error));
 }
 
-showWeatherOfCity('Москва')
-
+function showScheduleOfCity(name = ELEMENT_UI.SEARCH_CITY.value) {
+  fetchCity(name, "forecast").then((data) => {
+    ELEMENT_UI.SCHEDULES.forEach((item, index) => {
+      showTime(data.list[index].dt, ELEMENT_UI.SCHEDULE_TIMES[index]);
+      ELEMENT_UI.SCHEDULE_TEMP[index].textContent = Math.round(
+        data.list[index].main.temp
+      );
+      ELEMENT_UI.SCHEDULE_FEELS_LIKE[index].textContent = Math.round(
+        data.list[index].main.feels_like
+      );
+      ELEMENT_UI.SCHEDULE_ICON[
+        index
+      ].src = `https://openweathermap.org/img/wn/${data.list[index].weather[0].icon}@2x.png`;
+    });
+  });
+}
 
 function addFavouriteCities() {
   try {
@@ -45,6 +81,21 @@ function addFavouriteCities() {
     createFavoritesItem();
   } catch (error) {
     return alert(error);
+  }
+}
+
+function saveFavoriteCitiese() {
+  let row = JSON.parse(localStorage.getItem("favorite"));
+  if (row !== null) {
+    WEATHER_FAVOURITES.push(...row);
+  }
+}
+
+function saveСurrentCity() {
+  let aaa = localStorage.getItem("current");
+  if (aaa !== null) {
+    showWeatherOfCity(aaa);
+    showScheduleOfCity(aaa);
   }
 }
 
@@ -72,7 +123,9 @@ function useFavouriteCities() {
       if (target.classList.contains("favourites__city-name")) {
         target.parentNode.classList.add("use");
         const name = document.querySelector(".use").textContent;
+        localStorage.setItem("current", name);
         showWeatherOfCity(name);
+        showScheduleOfCity(name);
         createFavoritesItem();
       }
     });
@@ -84,14 +137,17 @@ function createFavoritesItem() {
   WEATHER_FAVOURITES.forEach((favourites) => {
     createElements(favourites.NAME_CITY_FAVOURITE);
   });
+  localStorage.setItem("favorite", JSON.stringify(WEATHER_FAVOURITES));
 }
 
 ELEMENT_UI.FORM.addEventListener("submit", (e) => {
+  localStorage.setItem("current", ELEMENT_UI.SEARCH_CITY.value);
   e.preventDefault();
   showWeatherOfCity();
-  deleteFavouriteCities();
-  useFavouriteCities();
+  showScheduleOfCity();
   ELEMENT_UI.SEARCH_CITY.value = "";
 });
 
 ELEMENT_UI.FAVOURITES_BTN.addEventListener("click", addFavouriteCities);
+
+callAllFunctions();
